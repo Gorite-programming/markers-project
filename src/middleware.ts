16c554d6f49c0e8046e05ee9bfebbf7582ee1940
gorite <tomato.tomato.sub@gmail.com>
2026-02-19 00:37:59 +0900

    Further refine middleware to fix sitemap HTML error by disabling localeDetection and improving matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,15 @@
+```javascript
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+export default createMiddleware({
+  ...routing,
+  // ブラウザの言語設定による自動リダイレクトをオフにする（SEO上のトラブルを防ぐため）
+  localeDetection: false
+});
 
 export const config = {
-    // sitemap.xml と robots.txt、および静的ファイルを対象外にする
-    matcher: ['/((?!api|_next|_vercel|sitemap\\.xml|robots\\.txt|[\\w-]+\\.\\w+).*)']
+  // sitemap.xml, robots.txt, favicon.ico, および静的ファイルを完全に除外
+  matcher: ['/((?!api|_next|_vercel|[\\w-]+\\.\\w+).*)']
 };
+```
